161094d2d5d219b01cbc2b6d62010af3b0d3f791
Core <seorisachs@gmail.com>
2013-10-28 23:53:43 -0400

    juciy platformer

---
+++ Assets/Scripts/GetAxisCode.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetAxisCode : MonoBehaviour {
+	
+		Vector3 inputVector;
+		float speed = 40f;
+		float move;
+		public AudioClip impact;
+        public float jumpForce;
+		public float turnSpeed;
+    
+        bool fall = true;
+        
+	
+	
+	
+        void Start () {
+        
+        }
+        
+        void Update () {
+                Vector3 horizontalSpeed = Vector3.Scale(rigidbody.velocity, new Vector3(1f,0f,1f));
+                
+                Ray groundTest = new Ray(transform.position, -transform.up);
+                RaycastHit groundTouch;
+                bool grounded;
+                
+                grounded = Physics.Raycast(groundTest, out groundTouch, (collider.transform.localScale.y / 2f) + .5f);
+                
+                if (Input.GetKey(KeyCode.A))
+                        transform.Rotate(Vector3.up, -turnSpeed);
+		
+                if (Input.GetKey(KeyCode.D)) 
+                        transform.Rotate(Vector3.up, turnSpeed);
+		
+				if(Input.GetAxis("Vertical") !=0)
+				{
+					move = Input.GetAxis("Vertical") * speed *Time.deltaTime;
+					transform.Translate(0,0, move);
+				}
+                
+                if (Input.GetKeyDown(KeyCode.Space) && grounded) {
+                        rigidbody.velocity.Scale(new Vector3(1f, 0f, 1f));
+                        rigidbody.AddForce(transform.up * jumpForce, ForceMode.Acceleration);
+                        grounded = fall = false;
+                      
+                }
+           
+        }
+	
+	void OnCollisionEnter(){
+	
+		audio.PlayOneShot(impact, 10f);
+	}
+}
---
+++ Assets/Scripts/Gmod8Platformer.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gmod8Platformer : MonoBehaviour {
+	
+	Vector3 jump = new Vector3 (0f, 20f, 0f); 
+	Vector3 rightmove = new Vector3 (10f, 0f, 0f);
+	Vector3 leftmove = new Vector3 (-10f, 0f, 0f);
+	Vector3 upmove = new Vector3 (0f, 0f, 10f);
+	Vector3 downmove = new Vector3 (0f, 0f, -10f);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Space))
+		{
+			transform.position += jump *Time.deltaTime;
+		}
+		
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.position += rightmove *Time.deltaTime;
+		}
+		
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.position += leftmove *Time.deltaTime;
+		}
+		
+		if (Input.GetKey(KeyCode.W))
+		{
+			transform.position += upmove *Time.deltaTime;
+		}
+		
+		if (Input.GetKey(KeyCode.S))
+		{
+			transform.position += downmove *Time.deltaTime;
+		}
+			
+	
+	}
+}
